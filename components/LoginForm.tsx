b43f59065134ce2a1eb948c72d4a2480885dc95b
setsubou <setsubou@protonmail.com>
2025-02-20 17:40:39 +0700

    Created separate login form

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,34 +1,37 @@
-"use client"
+"use client";
 
+import { useRouter } from "next/navigation";
 import { FormEvent } from "react";
 
 export default function LoginForm() {
-    async function handleLogin(event: FormEvent<HTMLFormElement>) {
-        event.preventDefault();
-    
-        const formData = new FormData(event.currentTarget);
-        const username = formData.get("username");
-        const password = formData.get("password");
-    
-        const response = await fetch('http://localhost:8888/auth/', {
-          method: "POST",
-          headers: {"Content-Type": "application/json"},
-          body: JSON.stringify({username, password})
-        })
-    
-        if (response.ok) {
-          console.log("Login successful");
-        } else {
-          console.log("Login failed");
-          console.log(response.status);
-        }
-      }
+  const router = useRouter();
 
-    return (
-        <form onSubmit={handleLogin} className="flex flex-col gap-2">
-          <input type="text" name="username" required/>
-          <input type="password" name="password" required/>
-          <button type="submit">Login</button>
-        </form>
-      );
+  async function handleLogin(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const username = formData.get("username");
+    const password = formData.get("password");
+
+    const response = await fetch("http://localhost:8888/auth/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+      credentials: "include"
+    });
+
+    if (response.ok) {
+      router.push("/movies");
+    } else {
+        location.reload()
+    }
+  }
+
+  return (
+    <form onSubmit={handleLogin} className="flex flex-col gap-2">
+      <input type="text" name="username" required />
+      <input type="password" name="password" required />
+      <button type="submit">Login</button>
+    </form>
+  );
 }
