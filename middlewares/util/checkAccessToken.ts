a662c93f5d76a347a7f8e66739822e61d550b7a2
setsubou <setsubou@protonmail.com>
2025-02-23 15:15:30 +0700

    Util to check validity of access token

--- middlewares/util/checkAccessToken.ts
+++ middlewares/util/checkAccessToken.ts
@@ -1,11 +1,13 @@
 // Check whether token is still valid or not
 
 import { cookies } from "next/headers";
+import { getApiUrl } from "../../src/util/envVar";
 
 export default async function checkAccessToken(): Promise<boolean> {
   const cookie = await cookies()
+  const apiUrl = getApiUrl()
 
-  const response = await fetch("http://localhost:8888/auth/verify-token", {
+  const response = await fetch(`${apiUrl}/auth/verify-token`, {
     method: "POST",
     headers: { "Cookie": cookie.toString()},
     credentials: "include",
